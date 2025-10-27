6e508f82b39c583553b5673bb2cd48d953eab8af
Majinyu13158 <u202213158@hust.edu.cn>
2025-10-27 19:24:45 +0800

    精简

--- SPR_Portable/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-SPR Sensor Analyst - 源代码包
-"""
-__version__ = "2.0.0-mvp"
-
--- SPR_Portable/src/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Controller层 - 控制器
-"""
-from .main_controller import MainController
-from .main_controller_full import MainControllerFull
-
-__all__ = ['MainController', 'MainControllerFull']
-
